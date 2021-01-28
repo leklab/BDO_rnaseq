7bb1f1b7b2a736eacf17fa50a76e480fb02ea4a9
nicole.lake@yale.edu <nicole.lake@yale.edu>
2021-01-28 15:41:14 -0500

    create repo

---
+++ BDO_deseq.R
@@ -0,0 +1,111 @@
+library(DESeq2)
+library(tximport)
+library(tidyverse)
+library(pheatmap)
+library(EnhancedVolcano)
+library(vsn)
+library(ggplot2)
+
+#format gtf
+gtf <- as_tibble(rtracklayer::import('gencode.v26.GRCh38.ERCC.genes.gtf'))
+gtf <- gtf %>% select(gene_id, gene_name) %>% distinct()
+
+#define samples
+samples <- read_tsv('groups.tsv', col_names = c('sample', 'group'))
+
+#comparisons
+samples_BvsA <- samples[samples$group=="Group_A_CTL-org" | samples$group=="Group_B_Th17",]
+samples_CvsA <- samples[samples$group=="Group_A_CTL-org" | samples$group=="Group_C_IL17-org",]
+
+#samples is relevant sample sheet, and comparison is the name of the comparison to be used in file saving
+#groups are which 2 groups to be compared, treated group first
+run_deseq <- function(samples,group1,group2,comparison){
+  print("import rsem files")
+  files <- file.path("RSEM_files", paste0(samples$sample, ".rsem.genes.results"))
+  names(files) <- samples$sample
+  txi.rsem <- tximport(files, type = "rsem", txIn = FALSE, txOut = FALSE)
+  
+  print("filter by TPM")
+  gene.list <- rownames(txi.rsem$abundance[rowMaxs(txi.rsem$abundance, value=FALSE)>5,])
+  txi.rsem$abundance <- txi.rsem$abundance[rowMaxs(txi.rsem$abundance, value=FALSE)>5,]
+  txi.rsem$counts <- subset(txi.rsem$counts, rownames(txi.rsem$counts) %in% gene.list)
+  txi.rsem$length <- subset(txi.rsem$length, rownames(txi.rsem$length) %in% gene.list)
+  
+  print("generate sample table")
+  sampleTable <- data.frame(condition = factor(samples$group))
+  rownames(sampleTable) <- colnames(txi.rsem$counts)
+  print(sampleTable)
+  
+  print("fix zero lengths")
+  txi.rsem$length[txi.rsem$length == 0] <- 1
+  
+  print("import to deseq2")
+  dds <- DESeqDataSetFromTximport(txi.rsem, sampleTable, ~condition)
+  
+  print("prefilter (remove very low expressed genes)")
+  keep <- rowSums(counts(dds)) >= 10
+  dds <- dds[keep,]
+  
+  print("set up comparisons")
+  #need to be levels = c("untreated","treated")
+  dds$condition <- factor(dds$condition, levels = c(group2,group1))
+  print(dds$condition)
+  
+  print("analyze")
+  dds <- DESeq(dds)
+  #for res, should be contrast=c("condition","treated","untreated")
+  res <- results(dds, alpha=0.05, contrast = c("condition",group1,group2)) #alpha is the adjusted p value cutoff, default is 0.1 
+  print(res)
+  print(summary(res))
+  sink(file = sprintf("/results/%s/%s_results_summary.txt",comparison,comparison))
+  summary(res)
+  sink(file = NULL)
+  
+  print("create heatmap")
+  rld = rlogTransformation(dds)
+  num.genes.plot <- ifelse((sum(res$padj < 0.05, na.rm=TRUE)<50),sum(res$padj < 0.05, na.rm=TRUE),50)
+  print(num.genes.plot)  
+  mat = assay(rld)[ head(order(res$padj),num.genes.plot), ] # select the top 50 genes with the lowest padj (or less if less significant)
+  mat = mat - rowMeans(mat) # Subtract the row means from each value
+  # Optional, but to make the plot nicer:
+  df = as.data.frame(colData(rld)[,c("condition")]) # Create a dataframe with a column of the conditions
+  colnames(df) = "condition" # Rename the column header
+  rownames(df) = colnames(mat) # add rownames
+  # and plot the actual heatmap
+  pheatmap(mat, annotation_col=df, show_rownames = F, legend = F, annotation_names_col = F, filename = sprintf("/results/%s/%s_heatmap.png",comparison,comparison))
+  
+  print("PCA plot")
+  vsd <- vst(dds, blind=FALSE)
+  plotPCA(vsd, intgroup="condition") + theme_bw()
+  ggsave(sprintf("/results/%s/%s_PCA.png",comparison,comparison), width = unit(7, 'in'), height  = unit(8, 'in'))
+  
+  print("save")
+  res <- as_tibble(res) %>% mutate(gene_id = rownames(res)) %>% left_join(gtf) %>% data.frame() #add gene name
+  resOrdered <- as.data.frame(res[order(res$pvalue),])
+  write.table(resOrdered, file = sprintf("/results/%s/%s_results.txt",comparison,comparison), append = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+  res.sig <- resOrdered[!is.na(resOrdered$padj) & (resOrdered$padj<0.05),]
+  write.table(res.sig, file = sprintf("/results/%s/%s_results_sig_only.txt",comparison,comparison), append = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+  print(nrow(res.sig))
+  
+  print("Volcano plot")
+  rownames(res) <- res$gene_id
+  
+  EnhancedVolcano(res,
+                  lab = res$gene_name,
+                  x = 'log2FoldChange',
+                  y = 'padj',#other option is padj
+                  xlim = c(-26, 26),
+                  title = sprintf("%s vs %s\n(plotting adjusted p-values)",group1,group2),
+                  pCutoff = 0.05, #10e-12 #default is 1e-05
+                  #FCcutoff = 2, #default is 1
+                  transcriptPointSize = 1.5,
+                  transcriptLabSize = 3.0,
+                  col=c('black', 'black', 'black', 'red3'),
+                  colAlpha = 1, 
+                  subtitle = NULL, 
+                  legendVisible = F)
+  ggsave(sprintf("/results/%s/%s_volcano.png",comparison,comparison), width = unit(7, 'in'), height  = unit(8, 'in'))
+}
+
+run_deseq(samples_BvsA,"Group_B_Th17","Group_A_CTL-org","GroupB_allvsGroupA")
+run_deseq(samples_CvsA,"Group_C_IL17-org","Group_A_CTL-org","GroupCvsGroupA")
---
+++ BDO_goenrichment.R
@@ -0,0 +1,94 @@
+library(DESeq2)
+library(tximport)
+library(tidyverse)
+library(goseq)
+library(org.Hs.eg.db)
+
+#format rsem files- remove version numbers, this is done from command line
+  #ls -1 *results | while read line
+  #do
+  #cat $line | sed 's/\(ENSG[0-9]*\)\.[0-9]*/\1/g' > $line.fix
+  #done
+##
+
+#samples file
+samples <- read_tsv('groups.tsv', col_names = c('sample', 'group'))
+
+#sample comparisons
+samples_BvsA <- samples[samples$group=="Group_A_CTL-org" | samples$group=="Group_B_Th17",]
+samples_CvsA <- samples[samples$group=="Group_A_CTL-org" | samples$group=="Group_C_IL17-org",]
+
+#needed for goseq
+#sed 's/\(ENSG[0-9]*\)\.[0-9]*/\1/g' gencode.v26.GRCh38.ERCC.genes.gtf > gencode.v26.GRCh38.ERCC.genes.fix.gtf
+txdb <- GenomicFeatures::makeTxDbFromGFF('gencode.v26.GRCh38.ERCC.genes.fix.gtf', format="gtf")
+txsByGene=GenomicFeatures::transcriptsBy(txdb,"gene")
+lengthData=median(width(txsByGene))
+
+run_goseq <- function(samples,group1,group2,comparison){
+  print("import rsem files")
+  files <- file.path("RSEM_files", paste0(samples$sample, ".rsem.genes.results.fix")) #notice, now it's the "fix" files, meaning without transcript versions
+  names(files) <- samples$sample
+  txi.rsem <- tximport(files, type = "rsem", txIn = FALSE, txOut = FALSE)
+  
+  print("generate sample table")
+  sampleTable <- data.frame(condition = factor(samples$group))
+  rownames(sampleTable) <- colnames(txi.rsem$counts)
+  print(sampleTable)
+  
+  print("fix zero lengths")
+  txi.rsem$length[txi.rsem$length == 0] <- 1
+  
+  print("filter by TPM")
+  gene.list <- rownames(txi.rsem$abundance[rowMaxs(txi.rsem$abundance, value=FALSE)>5,])
+  txi.rsem$abundance <- txi.rsem$abundance[rowMaxs(txi.rsem$abundance, value=FALSE)>5,]
+  txi.rsem$counts <- subset(txi.rsem$counts, rownames(txi.rsem$counts) %in% gene.list)
+  txi.rsem$length <- subset(txi.rsem$length, rownames(txi.rsem$length) %in% gene.list)
+  
+  print("import to deseq2")
+  dds <- DESeqDataSetFromTximport(txi.rsem, sampleTable, ~condition)
+  
+  print("prefilter (remove very low expressed genes)")
+  keep <- rowSums(counts(dds)) >= 10
+  dds <- dds[keep,]
+  
+  print("set up comparisons")
+  dds$condition <- factor(dds$condition, levels = c(group2,group1))
+  
+  print("analyze")
+  dds <- DESeq(dds)
+  res <- results(dds, alpha=0.05, contrast = c("condition",group1,group2)) #alpha is the adjusted p value cutoff, default is 0.1 
+
+  print("format for goseq")
+  assayed.genes <- rownames(res)
+  fdr.threshold <- 0.05
+  sig.genes <- res[!is.na(res$padj) & (res$padj < fdr.threshold),]
+  sig.genes.500 <- head(sig.genes[order(sig.genes$padj),],n=500)
+  #de.genes <- rownames(sig.genes)[ which(sig.genes$padj < fdr.threshold) ]
+  if (nrow(sig.genes) > 500) {
+    de.genes <- rownames(sig.genes.500)
+    } else { 
+      de.genes <- rownames(sig.genes)[ which(sig.genes$padj < fdr.threshold) ]
+    }
+  print(length(de.genes))
+  
+  gene.vector=as.integer(assayed.genes%in%de.genes)
+  names(gene.vector)=assayed.genes
+  head(gene.vector)
+  
+  lengthDataDE <- lengthData[intersect(names(lengthData),names(gene.vector))]
+  gene.vectorDE <- gene.vector[intersect(names(gene.vector),names(lengthData))]
+  
+  print("run goseq, quantify the length bias")
+  pwf=nullp(gene.vectorDE,"hg38","ensGene", bias.data = lengthDataDE)
+  print(head(pwf))
+  
+  print("run goseq, default method")
+  GO.wall=goseq(pwf,"hg38","ensGene")
+  print(head(GO.wall, n=20))
+  write.table(as.data.frame(GO.wall), file = sprintf("/results/%s/%s_GO.enrichment_results.txt",comparison,comparison), append = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+}
+
+run_goseq(samples_BvsA,"Group_B_Th17","Group_A_CTL-org","GroupB_allvsGroupA")
+run_goseq(samples_CvsA,"Group_C_IL17-org","Group_A_CTL-org","GroupCvsGroupA")
+
+
